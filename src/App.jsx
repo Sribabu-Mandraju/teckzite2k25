97ba85ad7c1ed33dfeb3969b418cd01877472e05
Sireesha <n210413@rguktn.ac.in>
2024-12-27 08:30:25 -0800

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReuseButton from './components/Buttons/ReuseButton';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import Footer from './components/Buttons/Footer';
+import Card from './components/Card/Card';
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ReuseButton/>}/>
-        <Route path="/footer" element={<Footer/>}/>
+        {/*<Route path="/" element={<ReuseButton/>}/>
+        <Route path="/footer" element={<Footer/>}/>*/}
+        <Route path="/" element={<Card/>}/>
       </Routes>
     </Router>
   )
--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -18,9 +18,9 @@ const Button = ({ name, text, action }) => {
     return (
         <div className="bg-[black] flex justify-center items-center">
             <div className="relative">
-                <img src={button} className="h-auto w-auto" />
+                <img src={button} className="h-[50px] w-[100px]" />
                 <div
-                    className="absolute text-[20px] font-bold text-white flex justify-center items-center z-[10px] top-[50px] right-[10px] left-[10px]"
+                    className="absolute text-[10px] font-bold text-white flex justify-center items-center z-[10px] top-[10px] right-[10px] left-[10px]"
                    onClick={handleClick}
                 >
                     {name}
