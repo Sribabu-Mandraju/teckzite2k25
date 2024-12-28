2f3ca9843e443a9a02197c236c153537a1f21039
Sribabu <sribabumandraju@gmail.com>
2024-12-28 20:42:34 +0530

    newchanges

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import Card from './components/Card/Card';
 import HorizontalCard from './components/Card/HorizontalCard';
 import './App.css'
-
+import { Menu } from './components/Menu';
 const App = () => {
   return (
+    <div className='bg-[#1E1C1C]'>
     <Router>
       <Routes>
-       <Route path="/" element={<HorizontalCard/>}/>
+       {/* <Route path="/" element={<HorizontalCard/>}/> */}
+              <Route path="/" element={<Menu/>}/>
+
       </Routes>
     </Router>
+    </div>
   )
 }
 
