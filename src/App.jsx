257af80eb1979e1fbf3740959173b4b47ddf64ae
Sireesha <n210413@rguktn.ac.in>
2024-12-29 06:18:05 -0800

    ji

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,21 @@ import './App.css'
 import Events from './components/Events/Events';
 import EventCard from './components/Events/EventCard';
 import HorizontalCard from './components/Card/HorizontalCard';
-import Referals from './components/Card/Referals';
 import Workshopcard from './components/workshops/Workshopcard';
 import WebTeam from './components/WebTeam/WebTeam';
+import Scanner from './components/Scanner/Scanner';
 const App = () => {
   return (
     <div className='bg-[#1E1C1C]'>
     <Router>
       <Routes>
         <Route path="/" element={<Workshopcard />}/>
-        <Route path="/referals" element={<Referals />}/>
         <Route path="/card" element={<EventCard />}/>
        <Route path="/horizontal" element={<HorizontalCard />}/>
        <Route path="/webteam" element={<WebTeam />}/>
        <Route path="/Events" element={<Events/>}/>
+       <Route path="/workshop" element={<Workshopcard/>}/>
+       <Route path="/scanner" element={<Scanner/>}/>
       </Routes>
     </Router>
     </div>
