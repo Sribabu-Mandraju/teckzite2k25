14ac6fda0d7c02b019c0d3adf98c758872f30fde
Yeluri Nakshatra <nakshatra.yeluri@gmail.com>
2024-12-30 23:49:42 +0530

    asdf

--- src/components/NewHeader/NewHeader.jsx
+++ src/components/NewHeader/NewHeader.jsx
@@ -6,34 +6,58 @@ const NewHeader = () => {
   return (
     <div>
       <div className="relative bg-black w-screen h-screen">
-        <div className="hidden lg:block">
-          <div className="z-[100] fixed top-[33px] left-0 w-full h-auto">
+        <div className="hidden lg:block ">
+          <div className="z-[100] fixed top-[26px] left-0 w-full h-auto">
             <img src={desktop2} className="w-full h-auto" />
           </div>
-          <div className="absolute w-full top-[20px] left-0 text-white flex flex-row justify-around">
+          <div className="fixed w-full h-auto top-[10px] left-0 text-white flex flex-row  justify-around">
             <div>Menu</div>
             <div className="text-5xl font-semibold">TECKZITE</div>
             <div>Login</div>
           </div>
+          <div className="z-[100] fixed bottom-[26px] right-0 w-full h-auto">
+            <img src={desktop2} className="w-full h-auto scale-y-[-1]" />
+          </div>
+          <div className="fixed w-full h-auto bottom-[0px] right-0 text-white flex flex-row items-center  justify-around">
+            <div>Instagram</div>
+            <div className="text-6xl font-semibold">TZ</div>
+            <div>Linkdin</div>
+          </div>
         </div>
         <div className="hidden md:block lg:hidden">
-          <div className="z-[100] fixed top-[33px] left-0 w-full h-auto">
+          <div className="z-[100] fixed top-[20px] left-0 w-full h-auto">
             <img src={tab2} className="w-full h-auto" />
           </div>
-          <div className="absolute w-full top-[20px] left-0 text-white flex flex-row justify-around">
+          <div className="absolute w-full top-[14px] left-0 text-white flex flex-row justify-around">
             <div>Menu</div>
             <div className="text-2xl font-semibold">TECKZITE</div>
             <div>Login</div>
           </div>
+          <div className="z-[100] fixed bottom-[25px] right-0 w-full h-auto">
+            <img src={desktop2} className="w-full h-auto scale-y-[-1]" />
+          </div>
+          <div className="fixed w-full h-auto bottom-0 right-0 text-white flex flex-row items-center justify-around">
+            <div>Instagram</div>
+            <div className="text-3xl font-semibold py-2">TZ</div>
+            <div>Linkdin</div>
+          </div>
         </div>
         <div className=" sm:block md:hidden">
-          <div className="z-[100] fixed top-[18px] left-0 w-full h-auto">
+          <div className="z-[100] fixed top-[8px] left-0 w-full h-auto">
             <img src={mobile2} className="w-full h-auto" />
           </div>
-          <div className="absolute w-full top-[10px] left-0 text-white flex flex-row justify-around">
-            <div className="text-sm">Menu</div>
-            <div className="text-xl font-semibold">TECKZITE</div>
-            <div className="text-sm">Login</div>
+          <div className="absolute w-full top-[6px] left-0 text-white flex flex-row justify-around">
+            <div className="text-[12px]">Menu</div>
+            <div className="text-lg font-semibold">TECKZITE</div>
+            <div className="text-[12px]">Login</div>
+          </div>
+          <div className="z-[100] fixed bottom-[18px] right-0 w-full h-auto">
+            <img src={desktop2} className="w-full h-auto scale-y-[-1]" />
+          </div>
+          <div className="fixed w-full h-auto bottom-0 right-0 text-white flex flex-row items-center justify-around">
+            <div className="text-[12px]">Instagram</div>
+            <div className="text-lg font-semibold ">TZ</div>
+            <div className="text-[12px]">Linkdin</div>
           </div>
         </div>
       </div>
