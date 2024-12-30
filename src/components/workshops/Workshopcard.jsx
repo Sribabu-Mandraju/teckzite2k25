59f94c071285ac7220d7090ba1a4e233f571c58d
BinduSree-18 <palukurthibindu18@gmail.com>
2024-12-30 17:08:16 +0530

    hover

--- src/components/workshops/Workshopcard.jsx
+++ src/components/workshops/Workshopcard.jsx
@@ -21,19 +21,19 @@ const Workshopcard = () => {
             <div className="absolute top-[30px] top-[20px] left-[45px] ">
                 <img 
                     src={anushka} 
-                    className="h-[225px] w-[200px] rounded-lg" 
+                    className="h-[225px] w-[200px] rounded-lg anush" 
                     alt="anushka" 
                 />
             </div>
             <div className="overlay text-white flex flex-row justify-center items-center gap-4">
-                <a href="#"><FaInstagram className="w-[30px] h-[30px]"/></a>
-                <a href="#"><RiFacebookCircleLine className="w-[30px] h-[30px]"/></a>
+                <a href="#"><FaInstagram className="w-[30px] h-[30px] text-[#0A69A5]"/></a>
+                <a href="#"><RiFacebookCircleLine className="w-[30px] h-[30px] text-[#0A69A5]"/></a>
                 
             </div>
 
             <div className="absolute top-[270px] left-[0px] flex flex-col gap-[20px] ">
                 <div className="flex flex-col gap-[20px] ml-[70px]">
-              <div className="text-[20px] text-[white] font-bold text-[white]  ">anushka</div>
+              <div className="text-[20px] text-[white] font-bold text-[white]  titlename">anushka</div>
               <div className="text-[15px] text-[white] text-center  ml-[-160px] ">4.00</div>
               </div>
               <div className=" ml-[160px]">
