6442c7fb377e16b6fc1a4ad9847767ff210d3040
Sireesha <n210413@rguktn.ac.in>
2025-01-02 01:28:57 -0800

    ji

--- src/components/Events/EventCard.jsx
+++ src/components/Events/EventCard.jsx
@@ -1,36 +1,57 @@
 import React from "react";
 import card from "../../assets/card.png";
-import anushka from "../../assets/anushka.jpeg";
+import anushka from '../../assets/anushka.jpeg';
 import Button from "../Buttons/Button";
+import { CardContainer, CardBody, CardItem } from "../../utils/3Dcard";
 
 const EventCard = ({ name, image, time }) => {
   return (
-    <div className="flex justify-center bg-[black] items-center ">
-      <div className=" relative">
-        <img src={card} className="relative h-[450px] w-[300px]" alt="card" />
-        <div className="absolute top-[30px] left-[45px] ">
+    <CardContainer className="flex justify-center items-center">
+      <CardBody className="relative h-[450px] w-[300px]">
+        {/* Background Card */}
+        <CardItem
+          translateZ={-20}
+          className="relative h-full w-full"
+        >
+          <img
+            src={card}
+            className="h-full w-full object-cover rounded-lg"
+            alt="card background"
+          />
+        </CardItem>
+
+        {/* Inner Content */}
+        <CardItem
+          translateZ={30}
+          className="absolute top-[30px] left-[45px] h-[225px] w-[200px]"
+        >
           <img
             src={image}
-            className="h-[225px] w-[200px] rounded-lg"
-            alt="anushka"
+            className="h-full w-full rounded-lg object-cover"
+            alt="event"
           />
-        </div>
+        </CardItem>
 
-        <div className="absolute top-[270px] left-[0px] flex flex-col gap-[20px] ">
+        <CardItem
+          translateZ={40}
+          className="absolute top-[270px] left-0 flex flex-col gap-[20px]"
+        >
           <div className="flex flex-col gap-[20px] ml-[70px]">
-            <div className="text-[20px] text-[white] font-bold text-[white]  ">
-              {name}
-            </div>
-            <div className="text-[15px] text-[white] text-center  ml-[-160px] ">
+            <div className="text-[20px] text-white font-bold">{name}</div>
+            <div className="text-[15px] text-white text-center ml-[-50px]">
               {time}
             </div>
           </div>
-          <div className=" ml-[160px]">
-            <Button name="know more" />
-          </div>
-        </div>
-      </div>
-    </div>
+        </CardItem>
+
+        <CardItem
+          translateZ={50}
+          className="absolute top-[350px] left-[160px]"
+        >
+          <Button name="know more" />
+        </CardItem>
+      </CardBody>
+    </CardContainer>
   );
 };
 
